b4c16bf32fd7d19783acd2bbcf8b5104c5497d7d
Tommy Chaniago <126432146+tommychan95@users.noreply.github.com>
2023-03-29 12:03:37 +0700

    Add files via upload
    Add Register TS

---
+++ Register.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Register</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>c2e2192c-415d-47b3-9cb5-87703d782461</testSuiteGuid>
+   <testCaseLink>
+      <guid>b349c93f-65d1-476f-a14a-ca3036ac2fcb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Step Definition/Register/REG001 - User want to create MobiPaid Account</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
