400d4c2d45d3756a497816f4510fbbb9d12508bb
Tommy Chaniago <126432146+tommychan95@users.noreply.github.com>
2023-04-22 14:45:29 +0700

    Add files via upload
    Add select country

---
+++ Object Repository/MobiPaid Register/Page_Register/select_Your country.rs
@@ -0,0 +1,251 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>select_Your country</name>
+   <tag></tag>
+   <elementGuidId>bf83ebd7-ea52-4345-88fe-3e17e547f782</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//select[@id='country']</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>#country</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>select</value>
+      <webElementGuid>dcf8022a-bce8-4ac6-bae8-c17ec9104977</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>name</name>
+      <type>Main</type>
+      <value>region</value>
+      <webElementGuid>9ef800e1-1ba6-40e2-b4b1-9a3f927abb1c</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>form-control border-grey</value>
+      <webElementGuid>e02b1509-a3bd-4134-8c6d-8ae8581f7397</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>country</value>
+      <webElementGuid>9b7ec954-5c51-4de1-8b16-55133129b2d3</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>onchange</name>
+      <type>Main</type>
+      <value>handle_state_change()</value>
+      <webElementGuid>2758b1c5-2af6-4b6f-bc1e-4519a19ae6f8</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+                  Your country
+                                       
+                      South Africa                    
+                                       
+                      United Kingdom                    
+                                       
+                      United States of America                    
+                                       
+                                                  
+                                       
+                      Botswana                    
+                                       
+                      Germany                    
+                                       
+                      Croatia                    
+                                       
+                      France                    
+                                       
+                      Kenya                    
+                                       
+                      Madagascar                    
+                                       
+                      Malta                    
+                                       
+                      Mauritius                    
+                                       
+                      Middle East                    
+                                       
+                      Netherlands                    
+                                       
+                      Switzerland                    
+                                       
+                      Tanzania                    
+                                       
+                      Zambia                    
+                                       
+                      Other Africa                    
+                                       
+                      Other Europe                    
+                                  </value>
+      <webElementGuid>2b26c1f9-86f8-429b-8cb0-b31f1638543b</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;country&quot;)</value>
+      <webElementGuid>645fe0cc-7e8f-4616-8712-2deb66621a6d</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:attributes</name>
+      <type>Main</type>
+      <value>//select[@id='country']</value>
+      <webElementGuid>8ce1fbdb-7ea0-476a-a53b-4a1e99757cf0</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//form[@id='formRegister']/div[5]/div[3]/select</value>
+      <webElementGuid>2b27aaf3-8e79-4618-97c2-585c83997b72</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Åland Islands'])[1]/following::select[1]</value>
+      <webElementGuid>eb6de176-1d2a-4543-8ea0-8a366e73263a</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Zimbabwe'])[1]/following::select[1]</value>
+      <webElementGuid>90e3a51a-99c1-4bf5-ba5d-79906efc8bad</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Payment Services Agreement'])[1]/preceding::select[2]</value>
+      <webElementGuid>7042f0c6-6bba-4d2c-963c-6c94d3c4f3ea</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[5]/div[3]/select</value>
+      <webElementGuid>d8af5cc6-476d-4f3a-966d-c3ed57e0fb09</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//select[@name = 'region' and @id = 'country' and (text() = '
+                  Your country
+                                       
+                      South Africa                    
+                                       
+                      United Kingdom                    
+                                       
+                      United States of America                    
+                                       
+                                                  
+                                       
+                      Botswana                    
+                                       
+                      Germany                    
+                                       
+                      Croatia                    
+                                       
+                      France                    
+                                       
+                      Kenya                    
+                                       
+                      Madagascar                    
+                                       
+                      Malta                    
+                                       
+                      Mauritius                    
+                                       
+                      Middle East                    
+                                       
+                      Netherlands                    
+                                       
+                      Switzerland                    
+                                       
+                      Tanzania                    
+                                       
+                      Zambia                    
+                                       
+                      Other Africa                    
+                                       
+                      Other Europe                    
+                                  ' or . = '
+                  Your country
+                                       
+                      South Africa                    
+                                       
+                      United Kingdom                    
+                                       
+                      United States of America                    
+                                       
+                                                  
+                                       
+                      Botswana                    
+                                       
+                      Germany                    
+                                       
+                      Croatia                    
+                                       
+                      France                    
+                                       
+                      Kenya                    
+                                       
+                      Madagascar                    
+                                       
+                      Malta                    
+                                       
+                      Mauritius                    
+                                       
+                      Middle East                    
+                                       
+                      Netherlands                    
+                                       
+                      Switzerland                    
+                                       
+                      Tanzania                    
+                                       
+                      Zambia                    
+                                       
+                      Other Africa                    
+                                       
+                      Other Europe                    
+                                  ')]</value>
+      <webElementGuid>6a713c21-602f-4622-9d2b-56209cf8b6d8</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
